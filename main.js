9b6f27768af08bac39b80f9586dd92d5e259759d
airpad <airpad94@gmail.com>
2024-11-07 22:56:13 -0500

    agregando vista de detalle de pokemon

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const pokemonList = document.getElementById("pokemon-list")
+const pokemonDetail = document.getElementById("pokemon-detail")
+const pokemonInfo = document.getElementById("pokemon-info")
+const backButton = document.getElementById("back-button")
 
 async function fetchPokemonData(pokemonId) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
     const pokemon = await response.json()
-    return pokemon    
+    return pokemon
 }
 
 function displayPokemon(pokemon){
@@ -14,14 +17,34 @@ function displayPokemon(pokemon){
     <h3>${pokemon.name.toUpperCase()}</h3>
     <p>ID: ${pokemon.id}</p>
     `
+    //linea para mostrar detalle de pokemon
+    pokemonCard.addEventListener("click",()=>showPokemonDetail(pokemon))
     pokemonList.appendChild(pokemonCard)
     return
 }
+backButton.addEventListener("click",()=>{
+    pokemonDetail.style.display = "none"
+    pokemonList.style.display = "grid"
+})
+function showPokemonDetail(pokemon){
+    pokemonList.style.display = "none"
+    pokemonDetail.style.display = "block"
+    pokemonInfo.innerHTML =`
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" >
+    <p>ID: ${pokemon.id} </p>
+    <p>Altura: ${pokemon.height} m</p>
+    <p>Peso: ${pokemon.weight} kg</p>
+    <p>Tipos: ${pokemon.types.map(p=>p.type.name).join(", ")}</p>
+    `
+    return
+}
+
 
 async function loadPokedex() {
     for (let i=1; i<=50; i++){
-    const pokemon = await fetchPokemonData(i)
-    displayPokemon(pokemon)
+        const pokemon = await fetchPokemonData(i)
+        displayPokemon(pokemon)
     }
     return
 }
